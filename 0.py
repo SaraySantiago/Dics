70c2fb5ab888c6208c29f7867695720495fa649c
SaraySantiago <ssantiago18@iesrioarba.es>
2024-11-05 10:43:56 +0100

    first commit

---
+++ 0.py
@@ -0,0 +1,8 @@
+
+L = [] # una lista es un contenedor de i. con la i. estructurada como simple sucesion de item, a la i se accede por posicion (minimo) pos -> valor
+d = {} # un diccionario es un contenedor de i. con la i. estructurada como simples parejas k-v desordenadas, a la i se accede por k (minimo) k -> valor
+
+
+
+if __name__ == "__main__": 
+    pass
